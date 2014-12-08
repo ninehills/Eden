f2d7710259ac5b72b08c8111fb8cdf727bc17352
thomashuang <lyanghwy@gmail.com>
2014-12-08 23:56:06 +0800

    Added cooment and root user

--- schema/schema.sql
+++ schema/schema.sql
@@ -3,17 +3,17 @@ SET character_set_client = utf8;
 
 
 CREATE TABLE `cron` (
-  `cron_id` INT(11) unsigned NOT NULL AUTO_INCREMENT,
-  `task_id` VARCHAR(64) DEFAULT NULL,
-  `name` VARCHAR(124) NOT NULL,
-  `action` VARCHAR(124) NOT NULL,
-  `data` TEXT DEFAULT NULL,
-  `event`  VARCHAR(64) NOT NULL,
-  `next_run` DATETIME DEFAULT NULL,
-  `last_run` DATETIME DEFAULT NULL,
-  `run_times` INT(11) unsigned NOT NULL,
-  `attempts` TINYINT DEFAULT 0 NOT NULL,
-  `status` TINYINT unsigned DEFAULT 0,
+  `cron_id` INT(11) unsigned NOT NULL AUTO_INCREMENT COMMENT 'cron id',
+  `task_id` VARCHAR(64) DEFAULT NULL COMMENT 'the task id for task claiming',
+  `name` VARCHAR(124) NOT NULL COMMENT 'the unique task name for human',
+  `action` VARCHAR(124) NOT NULL COMMENT 'the action name registed in app',
+  `data` TEXT DEFAULT NULL COMMENT 'The custum  json data arguments for app action',
+  `event`  VARCHAR(64) NOT NULL COMMENT 'The event pattern',
+  `next_run` DATETIME DEFAULT NULL COMMENT 'the task next run datatime',
+  `last_run` DATETIME DEFAULT NULL COMMENT 'The task last run datatime',
+  `run_times` INT(11) unsigned NOT NULL COMMENT 'the total run times of the current task',
+  `attempts` TINYINT DEFAULT 0 NOT NULL COMMENT 'The attempts times when task run failed',
+  `status` TINYINT unsigned DEFAULT 0 COMMENT 'the status of task (running, stop...)',
   `created` DATETIME NOT NULL DEFAULT NOW() COMMENT 'The DATETIME when the entry was created.',
   `last_five_logs` TEXT NOT NULL COMMENT 'the last five logs of the current task',
   
@@ -25,13 +25,13 @@ CREATE TABLE `cron` (
 
 DROP TABLE IF EXISTS `users`;
 CREATE TABLE `users` (
-  `uid` MEDIUMINT(8) unsigned NOT NULL AUTO_INCREMENT,
-  `username` varchar(140) NOT NULL,
-  `real_name` varchar(140) NOT NULL,
-  `email` VARCHAR(140) NOT NULL,
-  `password` CHAR(56) NOT NULL,
-  `status` enum('inactive','active', 'banned') NOT NULL,
-  `role` enum('root', 'administrator','user') NOT NULL,
+  `uid` MEDIUMINT(8) unsigned NOT NULL AUTO_INCREMENT COMMENT 'the unique user id',
+  `username` varchar(140) NOT NULL COMMENT 'the unique user name',
+  `real_name` varchar(140) NOT NULL COMMENT 'the nick name or real name',
+  `email` VARCHAR(140) NOT NULL COMMENT 'the email for the user',
+  `password` CHAR(56) NOT NULL COMMENT 'The sha224 encypted password',
+  `status` enum('actived', 'banned') NOT NULL 'The status of the current user',
+  `role` enum('root', 'administrator','user') NOT NULL 'The role of the current user',
   `created` DATETIME NOT NULL DEFAULT NOW() COMMENT 'The DATETIME when the user was created.',
 
   PRIMARY KEY (`uid`),
@@ -39,5 +39,7 @@ CREATE TABLE `users` (
 
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+INSERT INTO `users`(uid, username, real_name, email, password, status, role, created) VALUES(1, 'eden', 'Eden',
+'Eden@test.com', '5c92e783db7e7285450961fa3c510168ee81d27890f7d09b6664838d', 'actived', 'root', now());
 
 SET @@character_set_client = @saved_cs_client;
